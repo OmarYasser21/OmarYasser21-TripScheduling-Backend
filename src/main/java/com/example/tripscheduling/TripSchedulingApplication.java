f3676d4847a9f6429a66b43131c3381a2b3a2b16
OmarYasser21 <94626701+OmarYasser21@users.noreply.github.com>
2022-12-20 01:28:56 +0200

    Update TripSchedulingApplication.java

--- src/main/java/com/example/tripscheduling/TripSchedulingApplication.java
+++ src/main/java/com/example/tripscheduling/TripSchedulingApplication.java
@@ -27,7 +27,7 @@ public class TripSchedulingApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/*").allowedOrigins("*");
             }
         };
     }
